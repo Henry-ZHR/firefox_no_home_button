fcbafc27933c8345ee27be88c7ba00955dfe13a0
Henry-ZHR <henry-zhr@qq.com>
2022-08-03 17:14:27 +0800

    Refactor to avoid removing "back" action

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ An xposed module to prevent Firefox from showing home button in the toolbar
 
 Tested on:
 
-- Firefox (`org.mozilla.firefox`) 98.3.0
-- Firefox Beta (`org.mozilla.firefox_beta`) 99.0.0-beta.5
-- Firefox Nightly (`org.mozilla.fenix`) 100.0a1
+- Firefox (`org.mozilla.firefox`) 103.1.0
+- Firefox Beta (`org.mozilla.firefox_beta`) 104.0b3
+- Firefox Nightly (`org.mozilla.fenix`) 105.0a1
 
 (all from Google Play)
--- app/src/main/java/moe/henry_zhr/firefox_no_home_button/MainHook.java
+++ app/src/main/java/moe/henry_zhr/firefox_no_home_button/MainHook.java
@@ -2,8 +2,6 @@ package moe.henry_zhr.firefox_no_home_button;
 
 import androidx.annotation.Keep;
 
-import java.util.HashSet;
-
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
@@ -13,8 +11,6 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 public class MainHook implements IXposedHookLoadPackage {
   @Override
   public void handleLoadPackage(LoadPackageParam lpparam) {
-    final HashSet<Integer> set = new HashSet<>();
-
     XposedHelpers.findAndHookMethod(
         "mozilla.components.browser.toolbar.BrowserToolbar",
         lpparam.classLoader,
@@ -22,11 +18,11 @@ public class MainHook implements IXposedHookLoadPackage {
         "mozilla.components.concept.toolbar.Toolbar.Action",
         new XC_MethodHook() {
           @Override
-          protected void beforeHookedMethod(MethodHookParam param) {
-            // home button should be the first action added, so we just ignore the first action
-            final int id = System.identityHashCode(param.thisObject);
-            if (!set.contains(id)) {
-              set.add(id);
+          protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+            final Object action = param.args[0];
+            final Object listener = XposedHelpers.findField(action.getClass(), "listener").get(action);
+            // org.mozilla.fenix.browser.BrowserFragment$initializeUI$homeAction$1
+            if (listener.getClass().getName().contains("homeAction")) {
               param.setResult(null);
             }
           }
