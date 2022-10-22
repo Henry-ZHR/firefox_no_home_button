912d047c03fa6bbbe70a7a080f4393783a827ebb
Henry-ZHR <henry-zhr@qq.com>
2022-10-22 11:16:08 +0800

    Don't specify param type The param type of `addNavigationAction` seems to change from `Toolbar.Action` to `Toolbar.ActionButton`, but I can't find this change in their source code.

--- app/src/main/java/moe/henry_zhr/firefox_no_home_button/MainHook.java
+++ app/src/main/java/moe/henry_zhr/firefox_no_home_button/MainHook.java
@@ -1,21 +1,20 @@
 package moe.henry_zhr.firefox_no_home_button;
 
-import androidx.annotation.Keep;
-
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-@Keep
 public class MainHook implements IXposedHookLoadPackage {
   @Override
   public void handleLoadPackage(LoadPackageParam lpparam) {
-    XposedHelpers.findAndHookMethod(
-        "mozilla.components.browser.toolbar.BrowserToolbar",
-        lpparam.classLoader,
+    XposedBridge.hookAllMethods(
+        XposedHelpers.findClass(
+            "mozilla.components.browser.toolbar.BrowserToolbar",
+            lpparam.classLoader
+        ),
         "addNavigationAction",
-        "mozilla.components.concept.toolbar.Toolbar.Action",
         new XC_MethodHook() {
           @Override
           protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
