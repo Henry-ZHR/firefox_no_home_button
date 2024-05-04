5cb64aea87a2d3a911a314bf0aeaaad886e8eb47
Henry-ZHR <henry-zhr@qq.com>
2024-05-04 21:54:58 +0800

    Check `imageDrawable` which should be more stable

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 An xposed module to prevent Firefox from showing home button in the toolbar
 
-Note: since Firefox 119 (or maybe ±1)，[the newly added erase button](https://github.com/mozilla-mobile/firefox-android/pull/3145) won't show either
+~~Note: since Firefox 119 (or maybe ±1)，[the newly added erase button](https://github.com/mozilla-mobile/firefox-android/pull/3145) won't show either~~  
+Now it should only remove the "home" button.
 
 ---
 
 Tested on:
 
-- Firefox (`org.mozilla.firefox`) 103.1.0
-- Firefox Beta (`org.mozilla.firefox_beta`) 119.0b3
-- Firefox Nightly (`org.mozilla.fenix`) 120.0a1
+- Firefox (`org.mozilla.firefox`) `125.3.0 (Build #2016017559), 9f29904153+`
+- Firefox Beta (`org.mozilla.firefox_beta`) `126.0b9 (Build #2016018671), hg-7245a1e9161d+`
+- Firefox Nightly (`org.mozilla.fenix`) `127.0a1 (Build #2016018767), hg-7251b91515e4+`
 
 (all from Google Play)
--- app/src/main/java/moe/henry_zhr/firefox_no_home_button/MainHook.java
+++ app/src/main/java/moe/henry_zhr/firefox_no_home_button/MainHook.java
@@ -1,5 +1,13 @@
 package moe.henry_zhr.firefox_no_home_button;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.Set;
+import java.util.WeakHashMap;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
@@ -7,8 +15,37 @@ import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class MainHook implements IXposedHookLoadPackage {
+  private static final String[] HOME_DRAWABLE_NAMES = {
+      "mozac_ic_home",
+      "mozac_ic_home_24"
+  };
+
   @Override
   public void handleLoadPackage(LoadPackageParam lpparam) {
+    final Set<Drawable> drawables = Collections.synchronizedSet(
+        Collections.newSetFromMap(new WeakHashMap<>())
+    );
+
+    XposedHelpers.findAndHookMethod(
+        "androidx.appcompat.content.res.AppCompatResources",
+        lpparam.classLoader,
+        "getDrawable",
+        Context.class,
+        int.class,
+        new XC_MethodHook() {
+          @Override
+          protected void afterHookedMethod(MethodHookParam param) {
+            final Context context = (Context) param.args[0];
+            final String name = context.getResources().getResourceEntryName((int) param.args[1]);
+            for (String item : HOME_DRAWABLE_NAMES) {
+              if (name.equals(item)) {
+                drawables.add((Drawable) param.getResult());
+                break;
+              }
+            }
+          }
+        }
+    );
     XposedBridge.hookAllMethods(
         XposedHelpers.findClass(
             "mozilla.components.browser.toolbar.BrowserToolbar",
@@ -19,11 +56,8 @@ public class MainHook implements IXposedHookLoadPackage {
           @Override
           protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
             final Object action = param.args[0];
-            final Object listener = XposedHelpers.findField(action.getClass(), "listener").get(action);
-            final String name = listener.getClass().getName();
-            // org.mozilla.fenix.browser.BrowserFragment$initializeUI$homeAction$1
-            // org.mozilla.fenix.browser.BrowserFragment$initializeUI$leadingAction$2
-            if (name.contains("homeAction") || name.contains("leadingAction")) {
+            final Field field = XposedHelpers.findField(action.getClass(), "imageDrawable");
+            if (drawables.contains((Drawable) field.get(action))) {
               param.setResult(null);
             }
           }
